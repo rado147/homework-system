4cbcec55f511ccdf4bf359e7c279256deec3d95f
Radoslav Filipov <rado147@abv.bg>
2018-09-03 01:33:07 +0300

    Support downloading and uploading homeworks

--- php/download_file.php
+++ php/download_file.php
@@ -1,6 +1,7 @@
 <?php
 
 if(isset($_GET['download'])) {
+    
     $file = "files_repository/" . $_GET['file_name'];
 
     header("Cache-Control: public");
@@ -8,7 +9,16 @@ if(isset($_GET['download'])) {
     header("Content-Disposition: attachment; filename=$file");
     header("Content-Transfer-Encoding: binary");
 
-    readdir($file);
+    readfile($file);
+} else if(isset($_GET['download_homework'])) {
+    $homework = "homeworks_repository/" . $_GET['homework_name'];
+
+    header("Cache-Control: public");
+    header("Content-Description: File Transfer");
+    header("Content-Disposition: attachment; filename=$homework");
+    header("Content-Transfer-Encoding: binary");
+
+    readfile($homework);
 }
 
 ?>
